56e93d61a11abb970d487d55ce6786d24dc4bb07
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-05-13 15:58:02 +0800

    first commit

---
+++ car2lammpsdata/car2lammpsdata.py
@@ -0,0 +1,53 @@
+import argparse
+from ase.data import atomic_numbers, atomic_masses
+from collections import Counter
+
+
+def readcar(carfile):
+    crds = []
+    symbols = []
+    b = False
+    with open(carfile) as f:
+        for line in f:
+            s = line.split()
+            if s[0] == 'PBC':
+                box = list(map(float, s[1:4]))
+                b = True
+            elif b and len(s) == 9:
+                crds.append(list(map(float, s[1:4])))
+                symbols.append(s[-2])
+    return crds, symbols, box
+
+
+def write(datafile, crds, symbols, box):
+    buff = ['by Jinzhe Zeng']
+    buff.append(f'{len(symbols)} atoms')
+    symbollist = list(Counter(symbols))
+    buff.append(f'{len(symbollist)} atom types')
+    buff.extend([f'0 {box[i]} {d}lo {d}hi' for i,
+                 d in enumerate(['x', 'y', 'z'])])
+    buff.extend(['Masses', ''])
+    buff.extend([f'{i} {atomic_masses[atomic_numbers[s]]}' for i,
+                 s in enumerate(symbollist, 1)])
+    buff.extend(['Atoms', ''])
+    symbolindex = dict(zip(symbollist, range(1, len(symbollist)+1)))
+    buff.extend([f'{i} {symbolindex[s]} 0 {c[0]} {c[1]} {c[2]}' for i,
+                 (c, s) in enumerate(zip(crds, symbols), 1)])
+    with open(datafile, 'w') as f:
+        f.write('\n'.join(buff))
+
+
+def convert(carfile, datafile):
+    write(datafile, *readcar(carfile))
+
+
+def _commandline():
+    parser = argparse.ArgumentParser(description='car to lammps-data')
+    parser.add_argument('-i', '--carfile',  required=True)
+    parser.add_argument('-o', '--datafile',  required=True)
+    args = parser.parse_args()
+    convert(args.carfile, args.datafile)
+
+
+if __name__ == '__main__':
+    _commandline()
---
+++ setup.py
@@ -0,0 +1,36 @@
+"""Use 'pip install .' to install."""
+
+
+from setuptools import find_packages, setup
+
+if __name__ == '__main__':
+
+    setup(name='car2lammpsdata',
+          description='car2lammpsdata',
+          keywords="car2lammpsdata",
+          url='https://github.com/njzjz/car2lammpsdata',
+          author='Jinzhe Zeng',
+          author_email='jzzeng@stu.ecnu.edu.cn',
+          packages=find_packages(),
+          python_requires='~=3.3',
+          install_requires=[
+              'ase',
+          ],
+          entry_points={
+              'console_scripts': [
+                  'car2lammpsdata=car2lammpsdata.car2lammpsdata:_commandline',
+              ]
+          },
+          use_scm_version=True,
+          classifiers=[
+              "Natural Language :: English",
+              "Operating System :: POSIX :: Linux",
+              "Operating System :: Microsoft :: Windows",
+              "Programming Language :: Python :: 3.6",
+              "Programming Language :: Python :: 3.7",
+              "Topic :: Scientific/Engineering :: Chemistry",
+              "Topic :: Software Development :: Libraries :: Python Modules",
+              "Topic :: Software Development :: Version Control :: Git",
+          ],
+          zip_safe=True,
+          )
