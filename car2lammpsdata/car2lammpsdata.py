9db1f2c18be8f31d30202237092278d573418bdd
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-07-17 19:45:34 +0800

    fix: support writing atomic atom_style

--- car2lammpsdata/car2lammpsdata.py
+++ car2lammpsdata/car2lammpsdata.py
@@ -19,27 +19,33 @@ def readcar(carfile):
     return crds, symbols, box
 
 
-def write(datafile, crds, symbols, box, symbollist=None):
+def write(datafile, crds, symbols, box, atom_style="charge", symbollist=None):
     buff = ['by Jinzhe Zeng']
     buff.append(f'{len(symbols)} atoms')
     if symbollist is None:
         symbollist = list(Counter(symbols))
+    if atom_style == "charge":
+        charge = 0.
+    elif atom_style == "atomic":
+        charge = ""
+    else:
+        raise RuntimeError("Unsupported atom_style!")
     buff.append(f'{len(symbollist)} atom types')
-    buff.extend([f'0 {box[i]} {d}lo {d}hi' for i,
+    buff.extend([f'{box[i]} {d}lo {d}hi' for i,
                  d in enumerate(['x', 'y', 'z'])])
     buff.extend(['Masses', ''])
     buff.extend([f'{i} {atomic_masses[atomic_numbers[s]]}' for i,
                  s in enumerate(symbollist, 1)])
     buff.extend(['Atoms', ''])
     symbolindex = dict(zip(symbollist, range(1, len(symbollist)+1)))
-    buff.extend([f'{i} {symbolindex[s]} 0 {c[0]} {c[1]} {c[2]}' for i,
+    buff.extend([f'{i} {symbolindex[s]} {charge} {c[0]} {c[1]} {c[2]}' for i,
                  (c, s) in enumerate(zip(crds, symbols), 1)])
     with open(datafile, 'w') as f:
         f.write('\n'.join(buff))
 
 
-def convert(carfile, datafile, symbollist):
-    write(datafile, *readcar(carfile), symbollist)
+def convert(carfile, datafile, symbollist, atom_style="charge"):
+    write(datafile, *readcar(carfile), symbollist = symbollist, atom_style=atom_style)
 
 
 def _commandline():
@@ -47,8 +53,9 @@ def _commandline():
     parser.add_argument('-i', '--carfile',  required=True)
     parser.add_argument('-o', '--datafile',  required=True)
     parser.add_argument('-a', '-s', '--symbol', nargs='*')
+    parser.add_argument('--atom-style', default="charge")
     args = parser.parse_args()
-    convert(args.carfile, args.datafile, symbollist=args.symbol)
+    convert(args.carfile, args.datafile, symbollist=args.symbol, atom_style=args.atom_style)
 
 
 if __name__ == '__main__':
